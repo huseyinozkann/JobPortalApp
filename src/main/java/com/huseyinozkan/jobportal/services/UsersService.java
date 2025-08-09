efc58875efe157517e9dae55563c9a6ae1f2ba5c
hüseyinözkan <ozkan.hsyn1@gmail.com>
2025-08-09 22:40:21 +0300

    Users Service added

---
+++ src/main/java/com/huseyinozkan/jobportal/services/UsersService.java
@@ -0,0 +1,29 @@
+package com.huseyinozkan.jobportal.services;
+
+import com.huseyinozkan.jobportal.entity.Users;
+import com.huseyinozkan.jobportal.repository.UsersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.Optional;
+
+@Service
+public class UsersService {
+    private final UsersRepository usersRepository;
+
+    @Autowired
+    public UsersService(UsersRepository usersRepository) {
+        this.usersRepository = usersRepository;
+    }
+
+    public Users addNew(Users users){
+        users.setActive(true);
+        users.setRegistrationDate(new Date(System.currentTimeMillis()));
+        return usersRepository.save(users);
+    }
+
+    public Optional<Users> getUserByEmail(String email){
+        return usersRepository.findByEmail(email);
+    }
+}
