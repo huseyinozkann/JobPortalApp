dfd8cfb73e8dac9d73e2550070145f84efd55457
hüseyinözkan <ozkan.hsyn1@gmail.com>
2025-08-10 20:10:58 +0300

    Job Seeker, Recruiter and Skills added

---
+++ src/main/java/com/huseyinozkan/jobportal/repository/RecruiterProfileRepository.java
@@ -0,0 +1,7 @@
+package com.huseyinozkan.jobportal.repository;
+
+import com.huseyinozkan.jobportal.entity.RecruiterProfile;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RecruiterProfileRepository extends JpaRepository<RecruiterProfile, Integer> {
+}
