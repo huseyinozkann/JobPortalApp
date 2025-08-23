f77c9d0fc5dd0ea658382854f7358fe706d14d7c
hüseyinözkan <ozkan.hsyn1@gmail.com>
2025-08-24 00:17:41 +0300

    Recruiter Profile - Controller and Service created

---
+++ src/main/java/com/huseyinozkan/jobportal/services/RecruiterProfileService.java
@@ -0,0 +1,22 @@
+package com.huseyinozkan.jobportal.services;
+
+import com.huseyinozkan.jobportal.entity.RecruiterProfile;
+import com.huseyinozkan.jobportal.repository.RecruiterProfileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class RecruiterProfileService {
+    private final RecruiterProfileRepository recruiterProfileRepository;
+
+    @Autowired
+    public RecruiterProfileService(RecruiterProfileRepository recruiterProfileRepository) {
+        this.recruiterProfileRepository = recruiterProfileRepository;
+    }
+
+    public Optional<RecruiterProfile> getOne(Integer id) {
+        return  recruiterProfileRepository.findById(id);
+    }
+}
